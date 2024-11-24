10392d3d938bb4d23c0001a89db7d0e1161cd7be
PratamaAngga <anggashillnoir@gmail.com>
2024-11-24 08:15:08 +0700

    Experiment 1 after question

--- Experiment/Cinema20.java
+++ Experiment/Cinema20.java
@@ -10,10 +10,21 @@ public class Cinema20 {
         audience[2][0] = "Eka";
         audience[2][1] = "Farhan";
         audience[3][0] = "Gisel";
+        audience[3][1] = "Hana";
 
         System.out.printf("%s \t %s\n", audience[0][0], audience[0][1]);
         System.out.printf("%s \t %s\n", audience[1][0], audience[1][1]);
         System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
         System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
+
+        System.out.println(audience.length);
+        for (String[] rowAudience : audience) {
+            System.out.println("Length of row: " + rowAudience.length);
+        }
+
+        System.out.println("Audiences in the row 3: ");
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Audience in the row: " + (i + 1) + String.join(", ", audience[i]));
+        }
     }
 }
